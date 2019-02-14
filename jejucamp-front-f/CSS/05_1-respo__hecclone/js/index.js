a638d2fb77ebd572db8c963456fcc9c4b15cb075
bill <bill@billui-MacBookPro.local>
2019-02-14 22:47:06 +0900

    hecster.io 클론위한 폴더 생성 , 네비 반응형으로 모양완성, 로그인 여부에 따라서 다른 모양의 크리링 메뉴

---
+++ jejucamp-front-f/CSS/05_1-respo__hecclone/js/index.js
@@ -0,0 +1,14 @@
+(function() {
+
+    var menuBtn = document.querySelector('.global-menu-btn');
+
+    menuBtn.addEventListener('click', function() {
+        // document.body.classList.toggle('menu-on');
+        // document.body.classList.add('menu-on');
+        // document.body.classList.remove('menu-on');
+        document.body.classList.toggle('menu-on');
+    });
+
+})();
+
+
